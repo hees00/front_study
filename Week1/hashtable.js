91650824ba42a370d11445cc7e2295aac631a8b6
hees00 <1p2h3s@naver.com>
2022-03-03 22:02:32 +0900

    week v2

--- Week1/hashtable.js
+++ Week1/hashtable.js
@@ -1,7 +1,7 @@
 // Hashtable은 key에 null을 허용하지 않지만, HashMap은 key에 null을 허용
 
 class myHashtable{
-    table = new Array(1000)
+    table = new Array(1000)  //얘를 메모리로 생각
     totalkey=[]
 
     constructor(array=[]) { 
@@ -29,10 +29,9 @@ class myHashtable{
     }
 
     delete(key){ 
-        this.table[this.hashing[key]] = null
+        this.table[this.hashing(key)] = null
 
         for(let i in this.totalkey){
-            
             if(this.totalkey[i]== key){
                 this.totalkey.splice(i,1);
                 break
@@ -42,7 +41,20 @@ class myHashtable{
     }
 
     hashing(key){
-        return key.charCodeAt()*3%1000
+
+        if(typeof(key)=="string"){
+            return key.charCodeAt()*3%1000    
+        }
+        else if(typeof(key)=="number"){
+            return 1000 % key
+        }
+        else{
+            alert("key값는 문자열과 숫자만 가능합니다")
+        }
+    }
+
+    size(){
+        return this.totalkey.length
     }
 
 }
